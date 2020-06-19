41a2c2955f7989286d398e6f4323d2fb68e659ba
Junfeng Zhang <dev@akii.me>
2020-06-20 01:46:25 +0800

    Add svg template to make svg loading from path more convenience.

---
+++ src/components/svg.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg">
+    <path
+      v-for="(path, index) in paths"
+      :key="index"
+      :fill="path.fill !== null ? path.fill : currentColor"
+      :d="path.path"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'svg-icon',
+  props: {
+    paths: Array,
+  },
+};
+</script>
