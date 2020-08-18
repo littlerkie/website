ea230eb94a21c4c763dc6f3a34a9a249a6f7764e
Junfeng Zhang <dev@akii.me>
2020-08-18 15:17:20 +0800

    Add proj grid and grid tile view..

---
+++ src/components/grid-tile.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="d-flex flex-column h-100">
+    <a
+      :href="content.url"
+      class="d-block bar-lg overflow-hidden imgfill box-shadow-md position-relative"
+    >
+      <img
+        class="w-100 h-100 d-block"
+        :src="content.imageUrl"
+        alt=""
+        loading="lazy"
+      />
+    </a>
+    <header class="mt-4 mt-sm-3">
+      <section class="mb-3">
+        <a
+          :href="content.tag.url"
+          class="s-tag bar-md va-middle mr-2"
+          v-if="content.tag != null"
+          >{{ content.tag.name }}
+        </a>
+        <span class="text-uppercase">
+          {{ content.time }}
+        </span>
+      </section>
+      <h2 class="">
+        <a
+          :href="content.url"
+          :title="content.title"
+          rel="bookmark"
+          class="fc-black-800 h:fc-blue-500"
+        >
+          {{ content.title }}
+        </a>
+      </h2>
+    </header>
+    <div class="mb-4">
+      {{ content.summary }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'grid-tile',
+  props: {
+    content: Object,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "../assets/styles/body";
+
+.box-shadow-md {
+  box-shadow: var(--shadow-md);
+  &:hover {
+    box-shadow: var(--shadow-lg);
+  }
+}
+
+.bar-lg {
+  border-radius: 7px !important;
+}
+
+.imgfill {
+    padding-top: 52.3%;
+}
+
+.imgfill img {
+  object-fit: cover;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+}
+
+</style>
--- src/views/resume/resume.vue
+++ src/views/resume/resume.vue
@@ -1,14 +1,13 @@
 <template>
   <div id="resume">
     <b-navbar toggleable="lg" type="dark" variant="primary">
-      <b-navbar-brand href="#resume">
+      <b-navbar-brand>
         <span class="d-block d-lg-none">APPL</span>
         <span class="d-none d-lg-block">
-          <img
+          <!-- <img
             class="img-fluid img-profile rounded-circle mx-auto mb-2"
-            src="@/assets/img/profile.jpg"
-            alt
-          />
+            :src="modules.filter((mdl) => mdl.id === 'profile').avatar_url"
+          /> -->
         </span>
       </b-navbar-brand>
       <b-button v-b-toggle.collapse class="navbar-toggler">
@@ -24,8 +23,8 @@
     </b-navbar>
 
     <div class="resume-wrapper" v-scroll-spy>
-      <section v-for="mdl in modules" :key="mdl.id" :id="mdl.id">
-        <div v-if="mdl.id === 'profile'" class="section-wrapper">
+      <section class="module" v-for="mdl in modules" :key="mdl.id">
+        <div v-if="mdl.id === 'profile'" class="module-wrapper">
           <h1 class="font-weight-bold text-uppercase display-2 mb-0">
             {{ mdl.profile.first_name }}
             <span class="text-primary">{{ mdl.profile.last_name }}</span>
@@ -46,32 +45,50 @@
             </a>
           </div>
         </div>
-        <div v-else-if="mdl.id === 'experiance'" class="section-wrapper">
+        <div v-else-if="mdl.id == 'projects'" class="module-wrapper">
+          <h1 class="font-weight-bold text-uppercase mb-5">{{ mdl.title }}</h1>
+          <div class="d-flex flex-wrap flex-column flex-md-row">
+            <div
+              class="grid-tile overflow-hidden p-4 pb-5 pb-sm-4"
+              v-for="proj in projs"
+              :key="proj.id"
+            >
+              <grid-tile :content="proj"> </grid-tile>
+            </div>
+          </div>
+        </div>
+        <div v-else-if="mdl.id === 'experiance'" class="module-wrapper">
           <h1 class="font-weight-bold text-uppercase mb-5">{{ mdl.title }}</h1>
           <div
             class="exp-wrapper"
             v-for="(exp, index) in sortedExpList(mdl.works)"
             :key="index"
           >
-            <div class="d-flex flex-column flex-md-row justify-content-between mb-3">
+            <div
+              class="d-flex flex-column flex-md-row justify-content-between mb-3"
+            >
               <h4>{{ exp.company_name }} • {{ exp.title }}</h4>
-              <span class="text-secondery">{{ exp.start_date }} - {{ exp.end_date }}</span>
+              <span class="text-secondery"
+                >{{ exp.start_date }} - {{ exp.end_date }}</span
+              >
             </div>
             <ul v-if="exp.responsibilities != null">
-              <li class="mb-3" v-for="(responsibility, index) in exp.responsibilities" :key="index">
+              <li
+                class="mb-3"
+                v-for="(responsibility, index) in exp.responsibilities"
+                :key="index"
+              >
                 {{ responsibility }}
               </li>
             </ul>
           </div>
         </div>
-        <div v-else-if="mdl.id === 'education'" class="section-wrapper">
+        <div v-else-if="mdl.id === 'education'" class="module-wrapper">
           <h1 class="font-weight-bold text-uppercase mb-5">{{ mdl.title }}</h1>
-          <div
-            class="exp-wrapper"
-            v-for="(exp, index) in mdl.edu"
-            :key="index"
-          >
-            <div class="d-flex flex-column flex-md-row justify-content-between mb-3">
+          <div class="exp-wrapper" v-for="(exp, index) in mdl.edu" :key="index">
+            <div
+              class="d-flex flex-column flex-md-row justify-content-between mb-3"
+            >
               <div class="flex-grow-1">
                 <h2 class="font-weight-bold mb-0">{{ exp.title }}</h2>
                 <div class="d-flex align-items-center">
@@ -95,45 +112,48 @@
             </div>
           </div>
         </div>
-        <div v-else-if="mdl.id === 'skills'" class="section-wrapper">
+        <div v-else-if="mdl.id === 'skills'" class="module-wrapper">
           <h1 class="font-weight-bold text-uppercase mb-5">{{ mdl.title }}</h1>
-          <h4 class="mb-3" v-if="mdl.skill.profesional != null">
-            Programming Languages &amp; Tools
-          </h4>
-          <ul class="list-inline xxLang-list">
-            <li
-              class="list-inline-item"
-              v-for="(skill, index) in mdl.skill.profesional"
-              :key="index"
-            >
-              <eli-svg
-                class="icon"
-                v-if="skill.icon != null"
-                :paths="skill.icon.paths"
-              ></eli-svg>
-              <span v-else class="font-weight-bold">{{ skill }}</span>
-            </li>
-          </ul>
-          <h4 class="mb-3" v-if="mdl.skill.workflow != null">Workflow</h4>
-          <ul class="mb-0">
-            <li v-for="(wkf, index) in mdl.skill.workflow" :key="index">
-              <span>{{ wkf }}</span>
-            </li>
-          </ul>
+          <div
+            class="professional"
+            v-if="mdl.skill && mdl.skill.profesional != null"
+          >
+            <h4 class="mb-3">
+              Programming Languages &amp; Tools
+            </h4>
+            <ul class="list-inline xxLang-list">
+              <li
+                class="list-inline-item"
+                v-for="(skill, index) in mdl.skill.profesional"
+                :key="index"
+              >
+                <eli-svg
+                  class="icon"
+                  v-if="skill.icon != null"
+                  :paths="skill.icon.paths"
+                ></eli-svg>
+                <span v-else class="font-weight-bold">{{ skill }}</span>
+              </li>
+            </ul>
+          </div>
+          <div class="workflow" v-if="mdl.skill && mdl.skill.workflow != null">
+            <h4 class="mb-3">Workflow</h4>
+            <ul class="mb-0">
+              <li v-for="(wkf, index) in mdl.skill.workflow" :key="index">
+                <span>{{ wkf }}</span>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div v-else-if="mdl.id === 'interests'" class="section-wrapper">
+        <div v-else-if="mdl.id === 'interests'" class="module-wrapper">
           <h1 class="font-weight-bold text-uppercase mb-5">{{ mdl.title }}</h1>
           <ul>
-            <li
-              class="mb-3"
-              v-for="(hobby, index) in mdl.hobbies"
-              :key="index"
-            >
+            <li class="mb-3" v-for="(hobby, index) in mdl.hobbies" :key="index">
               {{ hobby }}
             </li>
           </ul>
         </div>
-        <div v-else class="section-wrapper">
+        <div v-else class="module-wrapper">
           <h1 class="font-weight-bold text-uppercase mb-5">{{ mdl.title }}</h1>
         </div>
       </section>
@@ -143,21 +163,71 @@
 
 <script>
 import http from 'utils/task';
+import GridTile from 'components/grid-tile.vue';
 
 export default {
   name: 'resume',
+  components: {
+    'grid-tile': GridTile,
+  },
   data() {
     return {
       modules: [],
+      projs: [
+        {
+          id: '1',
+          url: '',
+          title:
+            'Stack Overflow for Teams has a new kind of content – Articles',
+          summary:
+            "A new tool for bringing your organization's essential knowledge together in an easy-to-search platform.",
+          imageUrl:
+            'https://149351115.v2.pressablecdn.com/wp-content/uploads/2020/08/Articles-blog-image-2048x1075.png',
+          time: 'AUGUST 10, 2020',
+        },
+        {
+          id: '2',
+          url: '',
+          title: 'The key components for building a React community',
+          summary:
+            "The React community grew organically thanks to its instant popularity. Here's how the folks shepherding that community ensure that everyone who wants to contribute is welcome to.",
+          imageUrl:
+            'https://149351115.v2.pressablecdn.com/wp-content/uploads/2020/08/Nabors.png',
+          time: 'AUGUST 6, 2020',
+        },
+        {
+          id: '3',
+          url: '',
+          title: 'The Overflow #34: WYSIWYG style',
+          summary:
+            'Welcome to ISSUE #34 of the Overflow! This week, we explore how to build a full-text search engine, ways to get involved in open source development, and how to force a right-click on a website that doesn’t want you to. Plus, the debut of Articles, a new form of post in Stack Overflow for Teams.',
+          imageUrl:
+            'https://149351115.v2.pressablecdn.com/wp-content/uploads/2019/09/The-Overflow-Blog.png',
+          time: 'AUGUST 17, 2020',
+        },
+        {
+          id: '4',
+          url: '',
+          title:
+            'Our Series E Funding – An Inflection Point to Accelerate the Realization of our Mission',
+          summary:
+            "This is my third in a series of quarterly CEO blog posts. I'm excited to share some very positive updates.",
+          imageUrl:
+            'https://149351115.v2.pressablecdn.com/wp-content/uploads/2020/07/stack-overflow-pc-july20-2048x1075.png',
+          time: 'JULY 28, 2020',
+        },
+      ],
     };
   },
   methods: {
     async onLoading() {
-      this.modules = await http('/users/akii/resume');
+      this.modules = await http('/users/littlerkie/resume');
     },
 
     sortedExpList(exp) {
-      return exp.slice().sort((lhs, rhs) => (lhs.start_date > rhs.start_date ? -1 : 1));
+      return exp
+        .slice()
+        .sort((lhs, rhs) => (lhs.start_date > rhs.start_date ? -1 : 1));
     },
   },
   mounted() {
@@ -167,7 +237,7 @@ export default {
 </script>
 
 <style lang="scss">
-@import 'node_modules/bootstrap/scss/bootstrap';
+@import "node_modules/bootstrap/scss/bootstrap";
 @import url("https://at.alicdn.com/t/font_1932202_s1pihrh03mo.css");
 
 $navbar-max-width: 17rem;
@@ -239,7 +309,7 @@ $navbar-max-width: 17rem;
     padding: 0 3rem;
   }
 
-  section {
+  .module {
     display: flex;
     align-items: center;
     padding: 5rem 0;
@@ -254,7 +324,7 @@ $navbar-max-width: 17rem;
       min-height: 100vh;
     }
 
-    .section-wrapper {
+    .module-wrapper {
       width: 100%;
     }
 
@@ -292,5 +362,13 @@ $navbar-max-width: 17rem;
   .list-inline-item:not(:last-child) {
     margin-right: 1.5rem;
   }
+}
+
+.grid-tile {
+  flex-basis: 33%;
+}
+
+.ff-row-wrap {
+  flex-flow: row wrap !important;
 }
 </style>
