76786c3618b5390a6953c6a15d0ffbfdf561c8a5
Junfeng Zhang <dev@akii.me>
2020-09-26 16:31:04 +0800

    Add prettier plugin for eslint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     "plugin:prettier/recommended",
     "plugin:nuxt/recommended",
   ],
+  plugins: ["prettier"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
