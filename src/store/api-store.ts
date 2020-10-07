34d91cfed1e213776b4b4fe231d329e193938209
Junfeng Zhang <dev@akii.me>
2020-10-07 13:48:49 +0800

    Add api preview view.

---
+++ src/store/api-store.ts
@@ -0,0 +1,33 @@
+import { Context } from "@nuxt/types";
+import { Action, Module, Mutation, VuexModule } from "vuex-module-decorators";
+import { isString } from "~/utils/inspect";
+import { Loadable } from "~/utils/loadable";
+
+@Module({ name: "pure-md-file-store", stateFactory: true, namespaced: true })
+export default class ApiStore extends VuexModule implements Loadable {
+  isLoading = false;
+  markdown: string = "";
+
+  @Mutation
+  setMarkdown(md?: string) {
+    this.markdown = isString(md) ? md! : "";
+  }
+
+  @Mutation
+  setLoadingState(isLoading: boolean) {
+    this.isLoading = isLoading;
+  }
+
+  @Action({ rawError: true })
+  async onLoading(ctx: Context) {
+    this.setLoadingState(true);
+    try {
+      const file: string = await ctx.app.$http.$get()
+      this.setMarkdown(file);
+      this.setLoadingState(false);
+    } catch (error) {
+      this.setLoadingState(false);
+      throw error;
+    }
+  }
+}
--- src/utils/store.ts
+++ src/utils/store.ts
@@ -4,17 +4,27 @@ import BlogListStore from "~/store/blog-list-store";
 import ResumeStore from "~/store/resume-store";
 import PureMDFileStore from "~/store/pure-md-file-store";
 import BlogStore from "~/store/blog-store";
+import ApiStore from "~/store/api-store";
 
 let resumeStore: ResumeStore;
 let pureMDFileStore: PureMDFileStore;
 let blogListStore: BlogListStore;
 let blogStore: BlogStore;
+let apiStore: ApiStore;
 
 function typedStore(store: Store<any>): void {
   resumeStore = getModule(ResumeStore, store);
   pureMDFileStore = getModule(PureMDFileStore, store);
   blogListStore = getModule(BlogListStore, store);
   blogStore = getModule(BlogStore, store);
+  apiStore = getModule(ApiStore, store);
 }
 
-export { typedStore, resumeStore, pureMDFileStore, blogListStore, blogStore };
+export {
+  typedStore,
+  resumeStore,
+  pureMDFileStore,
+  blogListStore,
+  blogStore,
+  apiStore,
+};
