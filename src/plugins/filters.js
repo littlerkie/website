056461bf992f8c80dcf72cf83df6b45bfa6a8d5d
Junfeng Zhang <dev@akii.me>
2020-10-08 15:54:19 +0800

    Clean code of filters.

--- src/plugins/filters.js
+++ src/plugins/filters.js
@@ -1,19 +1,14 @@
 import Vue from "vue";
 import marked from "marked";
 import PR from "highlight.js";
-import swift from "highlight.js/lib/languages/swift";
 
 PR.configure({
-  classPrefix: 'prettyprint__'     // don't append class prefix
+  classPrefix: 'prettyprint__'
 });
-
-PR.registerLanguage("swift", swift);
 
 function markup(markdown) {
   const options = {
     highlight: function (code, lang) {
-        console.log(lang, code);
-      // return prettyPrint.prettyPrint()
       return PR.highlight(lang, code).value;
     },
     langPrefix: "hljs prettyprint language-",
