8c47135b222d8f22c987cd8dfd0f0c1a441f9a26
Junfeng Zhang <dev@akii.me>
2020-10-29 18:06:10 +0800

    Add pure md resume view.

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,26 +1,30 @@
 import { Store } from "vuex";
 import { getModule } from "vuex-module-decorators";
+import ApiStore from "~/store/api-store";
+import BlogStore from "~/store/blog-store";
 import BlogListStore from "~/store/blog-list-store";
 import PureMDFileStore from "~/store/pure-md-file-store";
-import BlogStore from "~/store/blog-store";
-import ApiStore from "~/store/api-store";
+import PureMDResumeStore from "~/store/pure-md-resume-store";
 
-let pureMDFileStore: PureMDFileStore;
-let blogListStore: BlogListStore;
-let blogStore: BlogStore;
 let apiStore: ApiStore;
+let blogStore: BlogStore;
+let blogListStore: BlogListStore;
+let pureMDFileStore: PureMDFileStore;
+let pureMDResumeStore: PureMDResumeStore;
 
 function typedStore(store: Store<any>): void {
   pureMDFileStore = getModule(PureMDFileStore, store);
   blogListStore = getModule(BlogListStore, store);
   blogStore = getModule(BlogStore, store);
   apiStore = getModule(ApiStore, store);
+  pureMDResumeStore = getModule(PureMDResumeStore, store);
 }
 
 export {
   typedStore,
-  pureMDFileStore,
+  apiStore,
   blogListStore,
   blogStore,
-  apiStore,
+  pureMDFileStore,
+  pureMDResumeStore
 };
---
+++ src/views/resume/pure-md-resume.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">
+    <main role="main" class="main" :inner-html.prop="markdown | markup"></main>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "nuxt-property-decorator";
+import darkModeEnabled from "@/utils/dark-mode";
+import { pureMDResumeStore } from "~/store";
+import { Context } from '@nuxt/types';
+
+@Component
+export default class PureMDResumeView extends Vue {
+  get markdown(): string {
+    return pureMDResumeStore.markdown;
+  }
+
+  mounted() {
+    darkModeEnabled();
+  }
+
+  async asyncData(context: Context) {
+    try {
+      await pureMDResumeStore.onLoading(context);
+    } catch (error) {
+      context.error(error);
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+body {
+  font-size: 15px;
+  color: var(--black-900);
+}
+
+.main {
+  margin: 0 auto;
+  max-width: 798px;
+  padding: 5em 1rem 3em;
+
+  h1 {
+    font-size: 3rem;
+  }
+
+  em {
+    font-size: 0.8em;
+    color: var(--black-400);
+  }
+
+  code {
+    font-size: 0.9em;
+    color: var(--black-800);
+  }
+
+  ul {
+    @include list-unstyled();
+  }
+
+  hr {
+    margin-bottom: 1rem;
+  }
+}
+</style>
