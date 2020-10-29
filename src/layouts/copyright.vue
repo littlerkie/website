f84d52aca0d8eddca83ec08f87f3d0f56ab2ea63
Junfeng Zhang <dev@akii.me>
2020-10-29 17:32:48 +0800

    Add default © footer layout.

---
+++ src/layouts/copyright.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="©">
+    <Nuxt />
+    <footer role="contentinfo">
+      <p class="© txt-a--center">Copyright © 2020 Junfeng Zhang. All rights reserved.</p>
+    </footer>
+  </div>
+</template>
+
+<style lang="scss">
+.© {
+  margin: 2.5rem 0;
+}
+</style>
