442d2db46a436777ecf8bc253b71111bca4319bc
Junfeng Zhang <dev@akii.me>
2020-10-30 18:12:24 +0800

    Remove api preview view.

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,13 +1,11 @@
 import { Store } from "vuex";
 import { getModule } from "vuex-module-decorators";
-import ApiStore from "~/store/api-store";
 import BlogStore from "~/store/blog-store";
 import BlogListStore from "~/store/blog-list-store";
 import PureMDFileStore from "~/store/pure-md-file-store";
 import PureMDResumeStore from "~/store/pure-md-resume-store";
 import ResumeStore from "~/store/resume-store";
 
-let apiStore: ApiStore;
 let blogStore: BlogStore;
 let blogListStore: BlogListStore;
 let pureMDFileStore: PureMDFileStore;
@@ -18,14 +16,12 @@ function typedStore(store: Store<any>): void {
   pureMDFileStore = getModule(PureMDFileStore, store);
   blogListStore = getModule(BlogListStore, store);
   blogStore = getModule(BlogStore, store);
-  apiStore = getModule(ApiStore, store);
   pureMDResumeStore = getModule(PureMDResumeStore, store);
   resumeStore = getModule(ResumeStore, store);
 }
 
 export {
   typedStore,
-  apiStore,
   blogListStore,
   blogStore,
   pureMDFileStore,
